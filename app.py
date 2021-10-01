4c59fe9771de9021fabfb37b803c6b820d6f26b8
Andrei Tan <20182921+KnotUntied@users.noreply.github.com>
2021-10-01 13:17:35 +0800

    Try hello world

--- app.py
+++ app.py
@@ -1,165 +1,63 @@
-#!/usr/bin/env python
-
 import json
-import os
-import urllib
+import requests
+from datetime import datetime as dt
 
-from flask import Flask, request, make_response
+from dateutil.parser import isoparse
 
-# Flask app should start in global layout
+from flask import Flask, request, make_response, jsonify
+
+from apify_client import ApifyClient
+
 app = Flask(__name__)
+log = app.logger
+
+client = ApifyClient()
 
 
-@app.route('/webhook', methods=['POST'])
+@app.route('/', methods=['POST'])
 def webhook():
     req = request.get_json(silent=True, force=True)
+    try:
+        action = req.get('queryResult').get('action')
+    except AttributeError:
+        return 'json error'
 
-    print("Request:")
-    print(json.dumps(req, indent=4))
+    if action == 'coronavirus.confirmed_cases':
+        res = confirmed_cases(req)
+    else:
+        log.error('Unexpected action.')
 
-    # res = processRequest(req)
+    print('Action: ' + action)
+    print('Response: ' + res)
 
-    # res = json.dumps(res, indent=4)
-    # print(res)
-    # r = make_response(res)
-    # r.headers['Content-Type'] = 'application/json'
-    # return r
-
-
-def processRequest(req):
-    pass
-    # if req.get("result").get("action") != "yahooWeatherForecast":
-    #     return {}
-    # baseurl = "https://query.yahooapis.com/v1/public/yql?"
-    # yql_query = makeYqlQuery(req)
-    # if yql_query is None:
-    #     return {}
-    # yql_url = baseurl + urllib.urlencode({'q': yql_query}) + "&format=json"
-    # print(yql_url)
-
-    # result = urllib.urlopen(yql_url).read()
-    # print("yql result: ")
-    # print(result)
-
-    # data = json.loads(result)
-    # res = makeWebhookResult(data)
-    # return res
+    return make_response(jsonify({'fulfillmentText': res}))
 
 
-def makeYqlQuery(req):
-    pass
-    # result = req.get("result")
-    # parameters = result.get("parameters")
-    # city = parameters.get("geo-city")
-    # if city is None:
-    #     return None
+def confirmed_cases(req):
+    # data = requests.get(
+    #     'https://api.apify.com/v2/key-value-stores/lFItbkoNDXKeSWBBA/records/LATEST?disableRedirect=true'
+    # )
 
-    # return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
+    # data_json = data.json()
+    # data_confirmed = data_json.get('infected')
+    # data_updated = data_json.get('lastUpdatedAtApify')
+    # data_updated_iso = isoparse(data_updated)
+    # data_updated_tz = data_updated_iso.astimezone
+
+    # if data_confirmed:
+    #     response = f'According to the DOH, there have been {data_confirmed} confirmed cases as of {}'
+    # else:
+    #     response = 'We were unable to retrieve data from the DOH. Please try again.'
+
+    # return response
+    return 'Hello World!'
 
 
-def makeWebhookResult(data):
-    pass
-    # query = data.get('query')
-    # if query is None:
-    #     return {}
-
-    # result = query.get('results')
-    # if result is None:
-    #     return {}
-
-    # channel = result.get('channel')
-    # if channel is None:
-    #     return {}
-
-    # item = channel.get('item')
-    # location = channel.get('location')
-    # units = channel.get('units')
-    # if (location is None) or (item is None) or (units is None):
-    #     return {}
-
-    # condition = item.get('condition')
-    # if condition is None:
-    #     return {}
-
-    # # print(json.dumps(item, indent=4))
-
-    # speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
-    #          ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
-
-    # print("Response:")
-    # print(speech)
-
-    # slack_message = {
-    #     "text": speech,
-    #     "attachments": [
-    #         {
-    #             "title": channel.get('title'),
-    #             "title_link": channel.get('link'),
-    #             "color": "#36a64f",
-
-    #             "fields": [
-    #                 {
-    #                     "title": "Condition",
-    #                     "value": "Temp " + condition.get('temp') +
-    #                              " " + units.get('temperature'),
-    #                     "short": "false"
-    #                 },
-    #                 {
-    #                     "title": "Wind",
-    #                     "value": "Speed: " + channel.get('wind').get('speed') +
-    #                              ", direction: " + channel.get('wind').get('direction'),
-    #                     "short": "true"
-    #                 },
-    #                 {
-    #                     "title": "Atmosphere",
-    #                     "value": "Humidity " + channel.get('atmosphere').get('humidity') +
-    #                              " pressure " + channel.get('atmosphere').get('pressure'),
-    #                     "short": "true"
-    #                 }
-    #             ],
-
-    #             "thumb_url": "http://l.yimg.com/a/i/us/we/52/" + condition.get('code') + ".gif"
-    #         }
-    #     ]
-    # }
-
-    # facebook_message = {
-    #     "attachment": {
-    #         "type": "template",
-    #         "payload": {
-    #             "template_type": "generic",
-    #             "elements": [
-    #                 {
-    #                     "title": channel.get('title'),
-    #                     "image_url": "http://l.yimg.com/a/i/us/we/52/" + condition.get('code') + ".gif",
-    #                     "subtitle": speech,
-    #                     "buttons": [
-    #                         {
-    #                             "type": "web_url",
-    #                             "url": channel.get('link'),
-    #                             "title": "View Details"
-    #                         }
-    #                     ]
-    #                 }
-    #             ]
-    #         }
-    #     }
-    # }
-
-    # print(json.dumps(slack_message))
-
-    # return {
-    #     "speech": speech,
-    #     "displayText": speech,
-    #     "data": {"slack": slack_message, "facebook": facebook_message},
-    #     # "contextOut": [],
-    #     "source": "apiai-weather-webhook-sample"
-    # }
+# def _call_apify_api():
+#     return requests.get(
+#         'http://api.worldweatheronline.com/premium/v1/weather.ashx'
+#     )
 
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
-
-    print("Starting app on port %d" % (port))
-
-    app.run(debug=False, port=port, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
