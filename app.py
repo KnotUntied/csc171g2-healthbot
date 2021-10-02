b927d3360c126b07df140b2cf1ac716582c83c47
Andrei Tan <20182921+KnotUntied@users.noreply.github.com>
2021-10-02 14:00:42 +0800

    Add deaths, recovered, and active cases

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ from dateutil.parser import isoparse
 
 from flask import Flask, request, make_response, jsonify
 
-from apify_client import ApifyClient
+# from apify_client import ApifyClient
 
 app = Flask(__name__)
 log = app.logger
 
-client = ApifyClient()
+# client = ApifyClient()
 
-phTZObject = timezone(timedelta(hours=8), name="Malay Peninsula Standard Time")
+PH_TZOBJECT = timezone(timedelta(hours=8), name="Malay Peninsula Standard Time")
 
 @app.route('/', methods=['POST'])
 def webhook():
@@ -35,26 +35,39 @@ def webhook():
     return make_response(jsonify({'fulfillmentText': res}))
 
 
-def confirmed_cases(req):
+def _get_from_apify(val, name, period='total'):
     data = requests.get(
         'https://api.apify.com/v2/key-value-stores/lFItbkoNDXKeSWBBA/records/LATEST?disableRedirect=true'
     )
 
     data_json = data.json()
-    data_confirmed = data_json.get('infected')
-    data_updated = data_json.get('lastUpdatedAtApify')
-    data_updated_iso = isoparse(data_updated)
-    data_updated_tz = data_updated_iso.astimezone(phTZObject)
+    data_value = data_json.get(val)
+    # data_updated = data_json.get('lastUpdatedAtApify')
+    # data_updated_iso = isoparse(data_updated)
+    # data_updated_tz = data_updated_iso.astimezone(PH_TZOBJECT)
 
-    if data_confirmed:
-        response = (
-            f'According to the DOH, there have been {data_confirmed:,} confirmed cases '
-            f'as of {data_updated_tz.strftime("%B %d, %Y")}.'
-        )
+    if data_value:
+        if period == 'current':
+            response = f'According to the DOH, the current number of {name} for COVID-19 is {data_value:,}.'
+        else:
+            response = f'According to the DOH, the total number of {name} for COVID-19 is {data_value:,}.'
     else:
         response = 'We were unable to retrieve data from the DOH. Please try again.'
 
     return response
+
+
+def confirmed_cases(req):
+    return _get_from_apify('infected', 'confirmed cases')
+
+def deaths(req):
+    return _get_from_apify('deceased', 'deaths')
+
+def recovered(req):
+    return _get_from_apify('recovered', 'recoveries')
+
+def active_cases(req):
+    return _get_from_apify('activeCases', 'active cases', 'current')
 
 
 if __name__ == '__main__':
