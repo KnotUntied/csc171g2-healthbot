f3155d3e30c92b36e8b920e5354780377d31fc96
Andrei Tan <20182921+KnotUntied@users.noreply.github.com>
2021-10-16 21:35:37 +0800

    Make page responsive

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ def index():
     return '''
 <html>
     <head>
+        <meta name="viewport" content="width-device-width, initial-scale=1">
         <title>Healthbot</title>
     </head>
     <body>
